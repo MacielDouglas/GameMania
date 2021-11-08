42bf10caa3d59d1c8c2846029085d97938862e19
MacielDouglas <douglasmaciel.dm@gmail.com>
2021-11-08 17:36:11 -0300

    terminado o layout da página e adicionado responsividade

--- Js/script.js
+++ Js/script.js
@@ -1,44 +1,17 @@
-alert ('Exemplo de comando alert');
-console.log('Mensgem enviada pelo console');
-
-function Enviar() {
-
-    var email = document.getElementById("emailid");
-
-    if (email.value != "") {
-        alert('Obrigado por cadastrar o seu email: ' + email.value + '. Foi encaminhado com sucesso')
-    }
-}
+// função slide header 
 
 $(document).ready(function(){
 
-	$("#slide li:eq(0)").addClass("ativo").show()
-setInterval(slide,4000)
+    $("#slide li:eq(0)").addClass("ativo").show()
+
+setInterval(slide, 4000)
 
 function slide(){
     if($(".ativo").next().length){  //se houver, irá fazer algo
         $(".ativo").removeClass("ativo").hide().next().addClass("ativo").show()
-        }else {  //se não, irá retornar ao estado inicial do slide
-            $(".ativo").removeClass("ativo").hide()
-		    $("#slide li:eq(0)").addClass("ativo").show()
+    }else { //se não, irá retornar ao estado inicial do slide
+        $(".ativo").removeClass("ativo").hide()
+        $("#slide li:eq(0)").addClass("ativo").show()
     }
 }
 })
-
-// $( document ).ready(function() {
-
-//     // carrosel inicio
-//     $("#rotativo img:eq(0)").addClass("banner_ativo").show()
-
-//     setInterval(slide, 3000)
-
-//     function slide(){
-//         if( $(".banner_ativo").next().length) {
-//             $(".banner_ativo").removeClass("banner_ativo").hide().next().addClass("banner_ativo").show()
-
-//         } else {
-//             $(".banner_ativo").removeClass().hide()
-//             $("#rotativo img:eq(0)").addClass("banner_ativo").show()
-//         }
-//     }
-// })
